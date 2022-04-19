87952ab79f98afebd4ed1165e8021397da97c647
Aiden Blinn <apblinn@gmail.com>
2022-04-19 17:21:38 -0500

    handle reloads from nested routes

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../build')));
+app.get('/*', (req: any, res: any) => {
+  return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+});
+
 /**
  * start server
  */
